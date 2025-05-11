8a8dd668119b6502f016836cbd0156d0e8040375
danil <danilo.volles@outlook.com>
2025-05-11 17:29:18 -0300

    main class rename

--- src/main/kotlin/com/danilo/volles/astronomer/api/AstronomerApiApplication.kt
+++ src/main/kotlin/com/danilo/volles/astronomer/api/AstronomerApiApplication.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class ProfessorApiApplication
+class AstronomerApiApplication
 
 fun main(args: Array<String>) {
-    runApplication<ProfessorApiApplication>(*args)
+    runApplication<AstronomerApiApplication>(*args)
 }
