523391e14b7bd90f4847418d0022b8feea3ca24c
Danilo Volles <danilo.volles@outlook.com>
2025-06-06 18:16:22 -0300

    feat: Degree.fromString() refactor

--- src/main/kotlin/com/danilo/volles/astronomer/api/model/Degree.java
+++ src/main/kotlin/com/danilo/volles/astronomer/api/model/Degree.java
@@ -1,9 +1,20 @@
 package com.danilo.volles.astronomer.api.model;
 
+import com.danilo.volles.astronomer.api.exception.InvalidDegreeValueException;
+
+import java.util.Arrays;
+
 public enum Degree {
     BACHELOR,
     MASTER,
-    DOCTOR,
+    DOCTOR;
+
+    public static Degree fromString(final String value) {
+        return Arrays.stream(Degree.values())
+                .filter(degree -> degree.name().equalsIgnoreCase(value))
+                .findFirst()
+                .orElseThrow(InvalidDegreeValueException::new);
+    }
 }
 
 
